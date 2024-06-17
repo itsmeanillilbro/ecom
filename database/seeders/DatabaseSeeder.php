d2744bcd5cf6d4018d6c38f48fce7be1cf86a935
anilsubediii <subedia806@gmail.com>
2024-06-17 19:41:11 +0545

    db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'luffy',
+            'email' => 'admin@gmail.com',
+            'password' => password_hash('adminpass',PASSWORD_BCRYPT),
         ]);
     }
 }
